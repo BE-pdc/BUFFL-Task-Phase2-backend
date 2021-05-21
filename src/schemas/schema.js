fcf9c69ab8bf5a9c3514d88eec75770826436f29
Peter De Clercq <dclercqpeter@gmail.com>
2021-05-21 11:43:32 +0200

    surveys can be created, read, updated and deleted (backend)

--- src/schemas/schema.js
+++ src/schemas/schema.js
@@ -1,6 +1,16 @@
 const { buildSchema, GraphQLSchema } = require('graphql');
 
 module.exports = new buildSchema(`
+    input SurveyInput {
+        name: String
+        picURL: String
+        created: String
+        description: String
+        target: Int
+        responses: Int
+        status: Boolean
+    }
+
     type Survey {
         _id: ID!
         name: String!
@@ -18,7 +28,7 @@ module.exports = new buildSchema(`
 
     type Mutation {
         createSurvey(name: String!, picURL: String!, description: String, target: Int!, status: Boolean!): Survey
-        updateSurvey(name: String, picURL: String, description: String, target: Int, status: Boolean): Survey
+        updateSurvey(id: ID!, input: SurveyInput): Survey
         deleteSurvey(id: ID!): Survey
     }
 `);
